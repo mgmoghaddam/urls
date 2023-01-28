495ce4dea3a569048970ed6fa411b8228111f84b
mostafaganjian <m.ganjian@irfarabi.com>
2023-01-28 13:22:21 +0330

    swagger host changed

--- docs/docs.go
+++ docs/docs.go
@@ -82,7 +82,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8090",
+	Host:             "https://mgmo.tech",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "Swagger Example API",
