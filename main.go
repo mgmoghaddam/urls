eba21260d248e352b64f057781435cca6b4fbce2
mostafaganjian <m.ganjian@irfarabi.com>
2023-01-28 13:32:59 +0330

    swagger host changed

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func init() {
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host      https://mgmo.tech
+// @host      mgmo.tech
 // @BasePath  /api/v1
 func main() {
 	configs.InitViper()
